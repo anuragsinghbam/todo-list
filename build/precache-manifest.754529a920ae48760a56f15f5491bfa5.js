401675d464ca1dba8a87fdc2ec9e5fa6b65eb707
Anurag Singh <anuragsinghbam@gmail.com>
2021-02-21 17:19:16 +0530

    Initial Commit

---
+++ build/precache-manifest.754529a920ae48760a56f15f5491bfa5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17259a35e2b038b079e1236f8939da41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c8d0cdc8986b11c02a",
+    "url": "/static/js/2.0ea2811c.chunk.js"
+  },
+  {
+    "revision": "0df6201f8dd1584c0c98",
+    "url": "/static/js/main.f13224bd.chunk.js"
+  },
+  {
+    "revision": "bb603cdcecd78fe2119f",
+    "url": "/static/js/runtime-main.2a02be55.js"
+  }
+]);
